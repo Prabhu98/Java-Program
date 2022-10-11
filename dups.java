23491ad37aa4d9599d2caccbdaca9f8055e57107
Prabhu98 <41529342+Prabhu98@users.noreply.github.com>
2022-10-11 09:24:08 +0530

    Add files via upload

--- dups.java
+++ dups.java
@@ -8,6 +8,19 @@ public class dups {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        
+		
+		/*
+		 * Create Set interface and implements HashSet class
+		 * Iterate for each add names into another variable
+		 * Another variable add it to the set
+		 * Print the result of the set
+		 * 
+		 * 
+		 * 
+		 */
+		
+		
+		
 		String names[] = {"Tom","Jerry","Tom","Java","Java"};
 		
 		Set<String> hset = new HashSet<String>();
