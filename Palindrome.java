3431985268544f802644f0c6b25e133bd4df3f3b
Prabhu98 <41529342+Prabhu98@users.noreply.github.com>
2022-10-25 14:32:47 +0530

    Expand From center -Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,50 @@
+package ExpandFromCenter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Palindrome {
+
+	/*
+	 * @Test public void td1() { Assert.assertEquals(expandFromCenter("caac"),
+	 * true); // Assert.assertEquals(expandFromCenter_fromEdges("aba"), true); }
+	 */
+
+	@Test
+	public void td2() {
+		Assert.assertEquals(expandFromCenter("abcba"), true);
+		// Assert.assertEquals(expandFromCenter_fromEdges("abcba"), true);
+	}
+
+	public boolean expandFromCenter(String s) {
+		int left = s.length() / 2 - s.length() % 2, right = s.length() / 2 + s.length() % 2;
+
+		if (left == right)
+			left--;
+
+		while (left >= 0) {
+			if (s.charAt(left--) != s.charAt(right++))
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * ExpandFromCenter
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+
+	public boolean expandFromCenter_fromEdges(String s) {
+		int left = 0, right = s.length() - 1;
+
+		while (left < right) {
+			if (s.charAt(left++) != s.charAt(right--))
+				return false;
+		}
+		return true;
+	}
+
+}
