fd26ac86c7a08b27ed899ba0465df9f5ed3f1788
Prabhu98 <41529342+Prabhu98@users.noreply.github.com>
2022-11-09 10:53:04 +0530

    Day09Nov2022-HashMapConcepts-RemoveoneChar

---
+++ HashMapConcepts.java
@@ -0,0 +1,36 @@
+package TestLeaf;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import io.netty.handler.codec.http.HttpContentEncoder.Result;
+
+public class HashMapConcepts {
+
+	public static void main(String[] args) {
+		String s = "AppleAimAir";
+
+		Map<Character, Integer> hmap = new HashMap<Character, Integer>();
+
+		for (int i = 0; i < s.length(); i++) {
+			hmap.put(s.charAt(i), hmap.getOrDefault(s.charAt(i), 0) + 1);
+		}
+
+		for (Entry<Character, Integer> res : hmap.entrySet()) {
+			// Character key = res.getKey();
+			if (res.getKey() == 'A' || res.getKey() =='p') {
+				if (res.getValue() >= 1) {
+					hmap.put(res.getKey(), res.getValue() - 1);
+				} else {
+					hmap.remove(res.getKey());
+				}
+			}
+
+		}
+		 System.out.println(hmap);
+	}
+	
+
+}
