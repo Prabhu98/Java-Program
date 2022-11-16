9c43bd8be74c4303ef0fc48ca81f986283c36eb2
Prabhu98 <41529342+Prabhu98@users.noreply.github.com>
2022-11-16 12:52:55 +0530

    Add files via upload

--- BuddyStrings.java
+++ BuddyStrings.java
@@ -11,7 +11,7 @@ public class BuddyStrings {
 
 	@Test
 	public void td1() {
-		String s = "ab", goal = "ba";
+		String s = "aaaaaaabc", goal = "aaaaaaacb";
 		boolean buddyStrings = buddyStrings(s, goal);
 		System.out.println(buddyStrings);
 	}
@@ -23,18 +23,42 @@ public class BuddyStrings {
 		if (A.equals(B)) {
 			Set set = new HashSet();
 			for (char c : A.toCharArray()) {
-				if(set.contains(c)) return true;
+				if (set.contains(c))
+					return true;
 				set.add(c);
 			}
 			return false;
-		}else {
-			 List<Integer> list = new ArrayList<Integer>();
-			 for(int i=0;i<A.length();i++) {
-				 if(A.charAt(i) != B.charAt(i)){
-					 return list.add(i);
-				 }
-			 }
-			 return list.size()==2 && A.charAt(list.get(0)) == B.charAt(list.get(1)) &&  A.charAt(list.get(1)) == B.charAt(list.get(0));
+		} else {
+			List<Integer> list = new ArrayList<Integer>();
+			for (int i = 0; i < A.length(); i++) {
+				if (A.charAt(i) != B.charAt(i)) {
+					return list.add(i);
+				}
 			}
+			return list.size() == 2 && A.charAt(list.get(0)) == B.charAt(list.get(1))
+					&& A.charAt(list.get(1)) == B.charAt(list.get(0));
+		}
+	}
+
+	public boolean buddyStringsII(String A, String B) {
+		if (A.length() != B.length())
+			return false;
+		char[] c = A.toCharArray();
+		char[] b = B.toCharArray();
+
+		for (int i = 0; i < A.length() - 1; i++) { 
+			//a a a a a a abc
+			c[i] = A.charAt(i + 1); // a
+			c[i + 1] = A.charAt(i); // a
+		}
+		
+		String A1 = String.valueOf(c);
+		
+		if (A1.equals(B)) {
+			return true;
+		}
+
+		return false;
+
 	}
 }
